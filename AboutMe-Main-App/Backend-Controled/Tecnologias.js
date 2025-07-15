9a95270e65a4106315e3c9f6acea68e740b44345
Eduardo <eduao154@gmail.com>
2025-07-15 14:58:58 -0400

    Redux

--- AboutMe-Main-App/Backend-Controled/Tecnologias.js
+++ AboutMe-Main-App/Backend-Controled/Tecnologias.js
@@ -1,3 +1,4 @@
+import Redux from "../src/Images/redux.svg";
 import SQLServer from "../src/Images/SQL-SERVER.png";
 import PostgresSQL from "../src/Images/SQL.png";
 
@@ -6,7 +7,9 @@ import PostgresSQL from "../src/Images/SQL.png";
   { id: 1, icon: "fa-brands fa-html5", style: { color: "#E34C26" }, name: "HTML5" },
   { id: 2, icon: "fa-brands fa-css3-alt", style: { color: "#264DE4" }, name: "CSS3" },
   { id: 3, icon: "fa-brands fa-square-js", style: { color: "#F7DF1E" }, name: "JavaScript" },
-  { id: 4, icon: "fa-brands fa-react", style: { color: "#61DAFB" }, name: "React.js" }]
+  { id: 4, icon: "fa-brands fa-react", style: { color: "#61DAFB" }, name: "React.js" },
+  { id: 5, icon: Redux , style: { color: "#61DAFB" }, name: "Redux" }
+]
 
    export const backend=[
   { id: 1, icon: "fa-brands fa-node-js", style: { color: "#339933" }, name: "Node.js" },
