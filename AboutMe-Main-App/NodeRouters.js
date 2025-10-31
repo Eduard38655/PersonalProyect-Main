64ee966c08c4dc8c847b77ef140da679e5595a27
Eduardo <eduao154@gmail.com>
2025-10-30 21:43:52 -0400

    Commit - 30/10/2025

--- AboutMe-Main-App/NodeRouters.js
+++ AboutMe-Main-App/NodeRouters.js
@@ -5,8 +5,7 @@ import { createRequire } from "module";
 import path from "path";
 import { fileURLToPath } from "url";
 import SubmitMessage from "../AboutMe-Main-App/Backend-Controled/SubmitData.js";
-import Validar from "../AboutMe-Main-App/Backend-Controled/Translate.js";
-
+ 
 const app = express();
 const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
@@ -18,7 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", SubmitMessage);
-app.use("/", Validar );
-app.listen(3000, (req, res) => {
+ app.listen(3000, (req, res) => {
   console.log("Listening on port 3000");
 });
